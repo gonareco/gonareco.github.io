0bd948f151bb3fe6867c6c8c14f9b573f1b36887
gonareco <gonareco@gmail.com>
2025-06-19 00:24:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from google.oauth2.service_account import Credentials
 # ===== Estilos CSS personalizados =====
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=external_stylesheets)
+server = app.server
 
 # Configuración de estilos
 styles = {
